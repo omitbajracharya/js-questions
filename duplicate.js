424e5dbbb1a0cb73e4c108a760988fb34e290529
omitbajracharya <omitbajracharya@gmail.com>
2024-06-19 12:57:18 +0545

    first Question

---
+++ duplicate.js
@@ -0,0 +1,12 @@
+// remove the duplicates from the existing array
+
+const arr =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','a', 'b', 'c', 'd', 'e', 'f']
+
+
+
+
+function RemoveDuplicates(arr) {
+
+console.log("Removing duplicates",arr)
+
+}
